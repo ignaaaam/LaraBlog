8ac69d6eeb2f7d9497da072d3bc29121972d3d8e
iamatnuria <ignasiamat10@gmail.com>
2022-03-03 21:28:22 +0100

    Laravel Project first files

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<article>
+    <h1><a href="/post">My first post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus ullam provident dolores ipsum quod
+        molestiae modi fugiat adipisci quisquam fugit ad tempore temporibus quas aperiam nemo, laboriosam natus atque
+        odit? Sapiente, assumenda ratione explicabo ab quibusdam quia unde voluptas ducimus, sint mollitia possimus hic
+        ipsum id deserunt? Rerum earum ad recusandae amet praesentium mollitia sed, molestias nostrum eos perspiciatis
+        impedit ratione nobis magnam cumque deserunt ducimus saepe, sit quam fugiat a, provident explicabo? Ex
+        architecto quibusdam magni nemo inventore earum. Non vel veniam expedita fugit ipsa. Molestiae, quaerat
+        excepturi corrupti deserunt quisquam delectus nobis. Aliquid eum adipisci mollitia quam?</p>
+</article>
+<article>
+    <h1><a href="/post">My second post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus ullam provident dolores ipsum quod
+        molestiae modi fugiat adipisci quisquam fugit ad tempore temporibus quas aperiam nemo, laboriosam natus atque
+        odit? Sapiente, assumenda ratione explicabo ab quibusdam quia unde voluptas ducimus, sint mollitia possimus hic
+        ipsum id deserunt? Rerum earum ad recusandae amet praesentium mollitia sed, molestias nostrum eos perspiciatis
+        impedit ratione nobis magnam cumque deserunt ducimus saepe, sit quam fugiat a, provident explicabo? Ex
+        architecto quibusdam magni nemo inventore earum. Non vel veniam expedita fugit ipsa. Molestiae, quaerat
+        excepturi corrupti deserunt quisquam delectus nobis. Aliquid eum adipisci mollitia quam?</p>
+</article>
+<article>
+    <h1><a href="/post">My third post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus ullam provident dolores ipsum quod
+        molestiae modi fugiat adipisci quisquam fugit ad tempore temporibus quas aperiam nemo, laboriosam natus atque
+        odit? Sapiente, assumenda ratione explicabo ab quibusdam quia unde voluptas ducimus, sint mollitia possimus hic
+        ipsum id deserunt? Rerum earum ad recusandae amet praesentium mollitia sed, molestias nostrum eos perspiciatis
+        impedit ratione nobis magnam cumque deserunt ducimus saepe, sit quam fugiat a, provident explicabo? Ex
+        architecto quibusdam magni nemo inventore earum. Non vel veniam expedita fugit ipsa. Molestiae, quaerat
+        excepturi corrupti deserunt quisquam delectus nobis. Aliquid eum adipisci mollitia quam?</p>
+</article>
+
+<script src="/app.js"></script>
+</body>
+</html>
