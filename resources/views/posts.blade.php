0e2b8218724299fd0e70d2c0e81ad7a67346a9e2
iamatnuria <ignasiamat10@gmail.com>
2022-03-07 06:20:07 +0100

    Views design beign implemented & made it dynamic, components created, added logo. Post page created. Authentication pending.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,32 @@
 <x-layout>
-        @foreach ($posts as $post)
-            <article>
+    @include ('_posts-header')
 
-                <a href="/posts/{{ $post->slug }}">
-                    <h1>{{ $post->title }}</h1>
-                </a>
-                <p>
-                    By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-                </p>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
 
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
+
+
+    </main>
+
+{{--        @foreach ($posts as $post)--}}
+{{--            <article>--}}
+
+{{--                <a href="/posts/{{ $post->slug }}">--}}
+{{--                    <h1>{{ $post->title }}</h1>--}}
+{{--                </a>--}}
+{{--                <p>--}}
+{{--                    By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--                </p>--}}
+
+{{--                <div>--}}
+{{--                    {{ $post->excerpt }}--}}
+{{--                </div>--}}
+{{--            </article>--}}
+{{--        @endforeach--}}
 </x-layout>
 
 
