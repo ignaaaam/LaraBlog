a3eebccbaded1900b3f61661b1b92c218ad5f466
iamatnuria <ignasiamat10@gmail.com>
2022-03-10 21:39:20 +0100

    Pagination added & withQueryString added for pages to work with categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         $user = User::factory()->create([
            'name' => 'John Doe'
         ]);
-        Post::factory(100)->create([
+        Post::factory(35)->create([
             'user_id' => $user->id
         ]);
     }
