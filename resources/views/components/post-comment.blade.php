b9cbee14d7f77949f034bb031a96d2f46ba24f10
iamatnuria <ignasiamat10@gmail.com>
2022-03-14 19:04:20 +0100

    Panel component created, comment form done. Comment submit logic pending...

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,22 +1,24 @@
 @props(['comment'])
+<x-panel class="bg-gray-50">
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60">
-    </div>
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
 
-            <p class="text-xs">
-                Posted
-                <time>{{ $comment->created_at->diffForHumans() }}</time>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
